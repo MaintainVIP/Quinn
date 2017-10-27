2a9f41bbb4ab6d9a39f93d9ad6178c9991ebaae3
quinn <quinn643@163.com>
2017-10-27 17:15:31 +0800

    初始化工程

---
+++ src/com/quinn/domain/Server.java
@@ -0,0 +1,26 @@
+package com.quinn.domain;
+
+/**
+ * Created by dllo on 17/10/27.
+ */
+public class Server {
+
+    private int id;
+    private String name;
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dllo
+  Date: 17/10/27
+  Time: 上午9:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>主页</title>
+  </head>
+  <body>
+  <a href="playGame.action">选择大区</a>
+
+  </body>
+</html>
